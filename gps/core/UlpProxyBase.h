df165078b34dd0f32d15b7f0c2a93351182c1a36
Sultanxda <sultanxda@gmail.com>
2015-11-15 18:38:04 -0800

    Revert "bacon: Update GPS HAL"
    Not ready for this.
    
    This reverts commit 86c6169ef608a21441ba18dcd97c1b2fccc06965.
    
    # Conflicts:
    #	gps/core/UlpProxyBase.h
    #	gps/gps.conf
    #	gps/libloc_api_50001/Android.mk

--- gps/core/UlpProxyBase.h
+++ gps/core/UlpProxyBase.h
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2015, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2014, The Linux Foundation. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -58,7 +58,7 @@ public:
                                        LocPosTechMask loc_technology_mask) {
         return false;
     }
-    inline virtual bool reportSv(GnssSvStatus &svStatus,
+    inline virtual bool reportSv(GpsSvStatus &svStatus,
                                  GpsLocationExtended &locationExtended,
                                  void* svExt) {
         return false;
@@ -68,6 +68,16 @@ public:
     }
     inline virtual void setAdapter(LocAdapterBase* adapter) {}
     inline virtual void setCapabilities(unsigned long capabilities) {}
+    inline virtual bool reportBatchingSession(GpsExtBatchOptions &options,
+                                              bool active) {
+        return false;
+    }
+    inline virtual bool reportPositions(GpsExtLocation * locations,
+                                        int32_t number_of_locations,
+                                        enum loc_sess_status status,
+                                        LocPosTechMask techMask) {
+        return false;
+    }
 };
 
 } // namespace loc_core
